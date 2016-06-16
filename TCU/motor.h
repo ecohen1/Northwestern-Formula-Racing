bf363f808ceea0cccae73c4bb3871e3721f0c27e
Eli <elicohen2018@u.northwestern.edu>
2016-06-16 00:12:24 -0500

    optimized shift times for lincoln

--- TCU/motor.h
+++ TCU/motor.h
@@ -1,23 +1,22 @@
 void shift(){
 
   if (gear_req == 1){
-  //upshift
-    //immediately tells SW to reset shift request!
+    //upshift
+    //immediately tells SW to reset shift request
     gear_reset = 1;
     gear_req = 0;
     sendCAN();
     del = 0;
-    //if (gear == 1) del = 25;
-    //digitalWrite(clutchpin,HIGH);
-    //delay(d1);//btw shifter middle, clutch in
+
+    //flatshift: ign cut, then shift, then ign on
     digitalWrite(IGN_CUT, HIGH);
     delay(CUT_TO_SHIFT);
     digitalWrite(upshiftpin,HIGH);
-    delay(ud2+del);//btw shifter middle, clutch in
+    delay(ud2+del);
     digitalWrite(upshiftpin,LOW);
     delay(SHIFT_TO_ON);
     digitalWrite(IGN_CUT, LOW);
-    //delay(d3);//btw shifter middle, clutch in
+
     //increment gear, update SW data
     gear_reset = 1;
     gear_req = 0;
@@ -26,31 +25,34 @@ void shift(){
     sendCAN();
   }
   else if (gear_req == 2 && gear >= 1) {
-  //downshift
+    //downshift
+    //immediately tells SW to reset shift request!
     gear_reset = 1;
     gear_req = 0;
     sendCAN();
     del = 0;
-    //if (gear == 2) del = 100;
+
+    //clutch out, shift, clutch engage
     digitalWrite(clutchpin,HIGH);
     delay(dd1+del);
     digitalWrite(dnshiftpin,HIGH);
     delay(dd2+del);
     digitalWrite(dnshiftpin,LOW);
     delay(dd3+del);
-    gear_reset = 1;    
+
+    //increment gear, update SW data
+    gear_reset = 1;
     gear_req = 0;
     gear--;
     if (gear < 1) gear = 1;
-    sendCAN();  
+    sendCAN();
   }
   else {
     if (clutch_pos_req < 200){
-      digitalWrite(clutchpin,LOW);
+      digitalWrite(clutchpin,LOW);//clutch engage
     }
     else {
-      digitalWrite(clutchpin,HIGH);
-    }    
+      digitalWrite(clutchpin,HIGH);//clutch out
+    }
   }
-  
 }
